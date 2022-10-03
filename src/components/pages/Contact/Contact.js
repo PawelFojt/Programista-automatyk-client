244b1e40cea3ce12959838e02f85248ae9f00dbf
Paweł Fojt <pwfojt@gmail.com>
2022-10-03 18:34:34 +0200

    contact site

---
+++ src/components/pages/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+export default function Contact() {
+  return (
+    <div className={styles.contact}>
+      <h1>Kontakt</h1>
+      <h2 className={styles.info}>Email: pwfojt@gmail.com</h2>
+      <h2 className={styles.info}>Telefon: 794 367 015</h2>
+      
+    </div>
+    
+  );
+}
