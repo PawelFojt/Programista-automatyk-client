1532e310f2c99453ced94a5fda5c15f9ded7d5be
Paweł Fojt <pwfojt@gmail.com>
2022-10-11 19:00:40 +0200

    social media bug fix

--- src/components/UI/SocialMedia/SocialMedia.js
+++ src/components/UI/SocialMedia/SocialMedia.js
@@ -6,10 +6,10 @@ export default function SocialMedia() {
   return (
     <div className={styles.socialMedia}>
       <div className={styles.atom}><Atom border="small" /></div>
-      <a href="https://www.facebook.com/pwfojt"><i className={`${styles.icon} fa-brands fa-square-facebook`}></i></a>
-      <a href="https://twitter.com/Pawe01951938"><i className={`${styles.icon} fa-brands fa-square-twitter`}></i></a>
-      <a href="https://github.com/PawelFojt"><i className={`${styles.icon} fa-brands fa-square-github`}></i></a>
-      <a href="https://www.instagram.com/pawelfojt/"><i className={`${styles.icon} fa-brands fa-square-instagram`}></i></a>
+      <a href="https://www.facebook.com/pwfojt" className={styles.icon}><i className='fa-brands fa-square-facebook'></i></a>
+      <a href="https://twitter.com/Pawe01951938" className={styles.icon}><i className='fa-brands fa-square-twitter'></i></a>
+      <a href="https://github.com/PawelFojt" className={styles.icon}><i className='fa-brands fa-square-github'></i></a>
+      <a href="https://www.instagram.com/pawelfojt/" className={styles.icon}><i className='fa-brands fa-square-instagram'></i></a>
     </div>
   )
 }
